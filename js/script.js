516ec8f95cca249c3d08fcdcd111fabc248243d9
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-02 13:23:10 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/**
+ * Projet Name : Dynamic Form Processing with PHP
+ * URL: http://techstream.org/Web-Development/PHP/Dynamic-Form-Processing-with-PHP
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Tech Stream
+ * http://techstream.org
+ */
+
+function isNumber(evt) {
+  evt = (evt) ? evt : window.event;
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+  return true;
+}
+	
+function addRow(tableID) {
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+	if(rowCount < 100){							// limit the user from creating fields more than your limits
+		var row = table.insertRow(rowCount);
+		var colCount = table.rows[0].cells.length;
+		for(var i=0; i<colCount; i++) {
+			var newcell = row.insertCell(i);
+			newcell.innerHTML = table.rows[rowCount -1].cells[i].innerHTML;
+		}
+	}else{
+		 alert("Maximum record per EOR is 100.");
+			   
+	}
+}
+
+function deleteRow(tableID) {
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;	
+    alert(rowCount);	
+	for(var i=1; i<rowCount; i++) {
+		var row = table.rows[i];
+		var chkbox = row.cells[0].childNodes[0]; 
+        
+		if(null != chkbox && true == chkbox.checked) {
+			if(rowCount <= 2) { 						// limit the user from removing all the fields
+				alert("Cannot Remove all record.");
+				break;
+			} else {
+			table.deleteRow(i);
+			rowCount--;
+			i--; }
+		}
+	}
+}
