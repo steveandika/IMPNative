d777e0525f53b76b0e8c90cec0a61b8671a89f5f
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-05 07:51:34 +0700

    Update common.php

--- asset/libs/common.php
+++ asset/libs/common.php
@@ -28,85 +28,97 @@
 		}
 	}
 
-  function openDB() {
-    $server='192.168.1.5, 1435'; 
-    $dbSQL=mssql_connect($server, 'sa', 'Pswd120907');
-	$mssg="";
+	function openDB() 
+	{
+		$server='192.168.1.5, 1435'; 
+		$dbSQL=mssql_connect($server, 'sa', 'Pswd120907');
+		$mssg="";
   
-    if (!$dbSQL) { $mssg="Something when wrong while connection to main database"; } 
-    else { 
-	  mssql_select_db("CSSCY");
-	  $mssg="connected"; 
-	}
+		if (!$dbSQL) { $mssg="Something when wrong while connection to main database"; } 
+		else 
+		{ 
+			mssql_select_db("CSSCY");
+			$mssg="connected"; 
+		}
 	
-	return $mssg;
-  }
+		return $mssg;
+	}
   
-  function validateLogin() {
-    $username="";
-    $passwd="";
-	$validLogin=0;
-	$status="";
+	function validateLogin() 
+	{
+		$username="";
+		$passwd="";
+		$validLogin=0;
+		$status="";
 
-    if(isset($_POST["username"]) && isset($_POST["password"])) {
-      $username=$_POST["username"];
-	  $passwd=$_POST["password"];
-	  $status="accept";
+		if(isset($_POST["username"]) && isset($_POST["password"])) 
+	    {
+			$username=$_POST["username"];
+			$passwd=$_POST["password"];
+			$status="accept";
 		 
-      $specialChar=array("'","/","=","#","*","%","-","AND","OR"); 
-      if (in_array($username, $specialChar)) { $status="denied"; }
-	  if ($status=="accept") { 
-	    if (in_array($passwd, $specialChar)) { $status="denied"; }
-	  }
+			$specialChar=array("'","/","=","#","*","%","-","AND","OR"); 
+			if (in_array($username, $specialChar)) { $status="denied"; }
+			if ($status=="accept") 
+			{ 
+				if (in_array($passwd, $specialChar)) { $status="denied"; }
+			}
  
-      if ($status=="accept") {   
-	     $_SESSION['defurl'] = '//icons.pt-imp.com';
+			if ($status=="accept") 
+			{   
+				$_SESSION['defurl'] = '//icons.pt-imp.com';
 		 
-	    if ($username=="root" && $passwd="sps") {
-  		  $timeout=60*60*12;
-		  $_SESSION["uid"]=$username;		  
-		  $_SESSION["fullName"]="System Administrator";
-		  $_SESSION["allowInsert"]=1;
-		  $_SESSION["allowUpdate"]=1;
-		  $_SESSION["allowDelete"]=1;		   	 
-		  $_SESSION["time_out"]=$timeout;	  			
-		}
-        else if ($username=="IKPP" && $passwd="Sm@7219!") {	  		  
-          setcookie("cuserid", $username, time() + (86400 * 30),"/");
-		  setcookie("cusername", "IKPP Admin", time() + (86400 * 30),"/");	
-		  setcookie("isExternal", "y", time() + (86400 * 30),"/");	
-		  $_COOKIE["cuserid"]=$username;
-		  $_COOKIE["cusername"]="IKPP Admin";
-		  $_COOKIE["isExternal"]="y";
-		}
-        else {			
-          openDB();		
-          $query="Select a.userid, a.alInsert, a.alDelete, a.alEdit, b.completeName 
-		          from userProfile a Inner Join m_Employee b On b.empRegID=a.userID 
-	              where a.userID='$username' And accessKey='$passwd' And isActive=1";
-          $result=mssql_query($query);
-          if(mssql_num_rows($result) > 0) {
-		    $row=mssql_fetch_array($result);	
+				if ($username=="root" && $passwd="sps") 
+				{
+					$timeout=60*60*12;
+					$_SESSION["uid"]=$username;		  
+					$_SESSION["fullName"]="System Administrator";
+					$_SESSION["allowInsert"]=1;
+					$_SESSION["allowUpdate"]=1;
+					$_SESSION["allowDelete"]=1;		   	 
+					$_SESSION["time_out"]=$timeout;	  			
+				}
+				else if ($username=="IKPP" && $passwd="Sm@7219!") 
+				{	  		  
+					setcookie("cuserid", $username, time() + (86400 * 30),"/");
+					setcookie("cusername", "IKPP Admin", time() + (86400 * 30),"/");	
+					setcookie("isExternal", "y", time() + (86400 * 30),"/");	
+					$_COOKIE["cuserid"]=$username;
+					$_COOKIE["cusername"]="IKPP Admin";
+					$_COOKIE["isExternal"]="y";
+				}
+				else 
+				{			
+					openDB();		
+					$query="Select a.userid, a.alInsert, a.alDelete, a.alEdit, b.completeName 
+							from userProfile a Inner Join m_Employee b On b.empRegID=a.userID 
+							where a.userID='$username' And accessKey='$passwd' And isActive=1";
+					$result=mssql_query($query);
+					if(mssql_num_rows($result) > 0) 
+					{
+						$row=mssql_fetch_array($result);	
 		     
-			$timeout=60*60*12;
-		    $_SESSION["uid"]=strtoupper($username);
-		    $_SESSION["passwd"]=$passwd;
-		    $_SESSION["fullName"]=$row["completeName"];
-		    $_SESSION["allowInsert"]=$row["alInsert"];
-		    $_SESSION["allowUpdate"]=$row["alEdit"];
-		    $_SESSION["allowDelete"]=$row["alDelete"];		   	 
-		    $_SESSION["time_out"]=$timeout;	  
-		  }
-		  else { $status="denied"; }
+						$timeout=60*60*12;
+						$_SESSION["uid"]=strtoupper($username);
+						$_SESSION["passwd"]=$passwd;
+						$_SESSION["fullName"]=$row["completeName"];
+						$_SESSION["allowInsert"]=$row["alInsert"];
+						$_SESSION["allowUpdate"]=$row["alEdit"];
+						$_SESSION["allowDelete"]=$row["alDelete"];		   	 
+						$_SESSION["time_out"]=$timeout;	  
+					}
+					else 
+					{ $status="denied"; }
+				}
+			}		 
 		}
-	  }		 
-	}
 	
-	return $status;
-  }
+		return $status;
+	}
  
 	  
-	function removeCookie() {
+	function removeCookie() 
+	{
 		session_destroy();
 		
 		/*setcookie("cuserid", "", time()-3600, "/");
@@ -117,12 +129,14 @@
 		unset($_COOKIE["isExternal"]);	  	*/
 	  }
 	  
-	  function redirectToIndex() {
+	function redirectToIndex() 
+	{
 		$url="//icons.pt-imp.com/log-out"; 
 		echo "<script type='text/javascript'>location.replace('$url');</script>";   
-	  }	  
+	}	  
 	  
-	  function headerReport($reportTitle, $whName, $periode1, $periode2) {
+	function headerReport($reportTitle, $whName, $periode1, $periode2) 
+	{
 		$dsgn="<label style='font: 15px;font-weight:500;text-decoration:underline'>Repair ToolBox - Report</label>
 			   <div class='height-10'>&nbsp;</div>
 			   <h6><strong>".$reportTitle."</strong></h6>
@@ -134,7 +148,8 @@
 		return $dsgn; 
 	}	
 	  
-	function validUnitDigit($paramNoUnit) {
+	function validUnitDigit($paramNoUnit) 
+	{
 		$constPrefix = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
 		$constSuffix = array(10,12,13,14,15,16,17,18,19,20,21,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38);
 		$constMulti = array(1,2,4,8,16,32,64,128,256,512);
@@ -154,7 +169,8 @@
 		  }
 		}
 		
-		for($leap=4; $leap<=9; $leap++) {
+		for($leap=4; $leap<=9; $leap++) 
+		{
 		  $digitNoUnit = intval(substr($paramNoUnit, $leap,1));
 		  $calc = $calc +($digitNoUnit * $constMulti[$leap]);
 		}		
@@ -162,14 +178,18 @@
 		$resultDiv = (Floor($calc/11)) *11;
 		$lastDigit = strval(($calc - $resultDiv) %10);
 		$digitNoUnit = substr($paramNoUnit, 10,1);
-		if(Trim($lastDigit) != $digitNoUnit ) {
+		if(Trim($lastDigit) != $digitNoUnit ) 
+		{
 		  return 'Not standard Container digit was found. Should be '.substr($paramNoUnit, 0,10).'<strong> '.$lastDigit.'</strong>';
-		} else { 
+		} 
+		else 
+		{ 
 		  return 'OK'; 
 		}
 	}
 	  
-	function haveCustomerName($customerID) {
+	function haveCustomerName($customerID) 
+	{
 		$namaCustomer = '';
 		$srch="Select CASE WHEN shortName Is NULL THEN completeName ELSE shortName End As custName 
 			   From m_Customer Where custRegID='$customerID'";
@@ -181,9 +201,9 @@
 		mssql_free_result($rslt);	
 		return $namaCustomer;
 	}
-	  
-			   
-	function validMenuAccess($tagID) {
+	  			 
+	function validMenuAccess($tagID) 
+	{
 		 $jmlBrs=1;
 		 
 		 if((strtoupper($_SESSION["uid"])!="ROOT") && ($tagID != 'change_pswd')) {
@@ -201,7 +221,8 @@
 		 return $jmlBrs;
 	}
 
-	function newvalidMenuAccess($roleName,$tagID) {
+	function newvalidMenuAccess($roleName,$tagID) 
+	{
 		$jmlBrs=1;
 
 		$sql = "Select ".$roleName." from usersIMPRole where userID = '$tagID' and isEnable = 1 and ".$roleName."=1";
@@ -222,7 +243,8 @@
 		return $jmlBrs;
 	} 
 
-	function showMessage($msgIs, $type) {
+	function showMessage($msgIs, $type) 
+	{
 		$html  = '<div id="msgw" class="frame">';
 		 
 		if ($type == 'error'){
@@ -243,7 +265,8 @@
 		echo $html;
 	}	   
 	  
-	function floatMessage($msgIs) {	
+	function floatMessage($msgIs) 
+	{	
 		include ("floatmsg_confirmonly.php");
 
 		echo "<script>document.getElementById('floatMsg').style.display = 'block';</script>";
