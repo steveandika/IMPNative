2a84bc68e0eb52b93124030a4bec757f444dabf9
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-06 07:50:27 +0700

    Add files via upload

---
+++ log-out.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+  <link rel="shortcut icon" href="e-imp-asset/office.png" type="image/x-icon" />  
+  <link rel="stylesheet" type="text/css" href="e-imp-asset/css/master.css" />
+  <script src="e-imp/asset/js/modernizr.custom.js"></script>  
+  <script src="e-imp/asset/js/jquery.min.2.1.1.js"></script>      
+  <title>IMP | Integrated Container System</title>
+  <style>
+  </style>
+</head>
+
+<body >
+	<div class="w3-container">
+	
+		<div class="frameLogout">
+			<div class="height-20"></div>
+			<span style="font-size:20px!important; font-weight:500"> 
+				You have been log out from I-ConS. To start using the application <a href="//icons.pt-imp.com/">click this.</a>
+			</span>
+			<div class="height-20"></div>
+		</div>
+	
+   
+	</div>	 
+</body>
+</html> 
