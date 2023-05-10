2fe4de38a4bb88023736f67de65939f852f6462b
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-11 04:45:23 +0700

    Update mntreor.php

--- e-imp/fr/mntreor.php
+++ e-imp/fr/mntreor.php
@@ -3,21 +3,34 @@
 		<h1>Monitoring Estimate of Repair Billing</h1>
 		<div class="height-20"></div>
 		<div class="height-30" style="border-top: 1px solid #7a7a52"></div>
-		<div id="inside-wrapper">
-			<label style="font-weight:500">Estimate Date</label>
-            <input type="date" class="w3-input w3-border" name="estimate1" required />
-			<div class="height-10"></div>   
-            <label style="font-weight:500">until</label></td>
-			<input type="date" class="w3-input w3-border" name="estimate2" required />
-			<div class="height-10"></div>   
-			<label style="font-weight:500">Monitoring Type</label>
-			<select class="w3-input w3-border" name="HampName" required />
-				<option value="EoRIConS">Eor IConS belum ditagihkan</option>
-				<option value="EoRPDF">Eor Pelayaran belum ditagihkan</option>
-				<option value="EoRInv">Eor Sudah ditagihkan belum ada No. Invoice</option>
-				<option value="EoRIComplete">Lengkap</option>
-			</select>
-		</div>
+		<table class="w3-table">
+			<tr>
+				<td><label style="font-weight:500">Estimate Date</label></td>
+			</tr>
+			<tr>
+				<td><input type="date" name="estimate1" required /></td>
+			</tr>
+			<tr>
+				<td><label style="font-weight:500">until</label></td>
+			</tr>
+			<tr>
+				<td><input type="date" name="estimate2" required /></td>
+			</tr>
+			<tr>
+				<td><label style="font-weight:500">Monitoring Type</label></td>
+			</tr>
+			<tr>
+				<td>
+					<select class="w3-input w3-border border-radius-3" name="HampName" required />
+						<option value="EoRIConS">Eor IConS belum ditagihkan</option>
+						<option value="EoRPDF">Eor Pelayaran belum ditagihkan</option>
+						<option value="EoRInv">Eor Sudah ditagihkan belum ada No. Invoice</option>
+						<option value="EoRIComplete">Lengkap</option>
+					</select>
+				</td>
+			</tr>			
+		</table>
+
 		
 		<div class="height-30" style="border-bottom: 1px solid #7a7a52"></div>
 		<div class="height-20"></div>
