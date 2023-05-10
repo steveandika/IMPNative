059d4738ea5485c5269551e2b997529569fe3104
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-11 04:01:33 +0700

    Update mntreor.php

--- e-imp/fr/mntreor.php
+++ e-imp/fr/mntreor.php
@@ -1,13 +1,17 @@
-    <form class="rpt border-radius-3" id="summaryHamparan" method="post"> 
-		<h1>Daily Summary Hamparan</h1>
+	<div class="height-10"></div>
+	<form class="rpt border-radius-3" id="summaryHamparan" method="post"> 
+		<h1>Monitoring Estimate of Repair Billing</h1>
 		<div class="height-20"></div>
 		<div class="height-30" style="border-top: 1px solid #7a7a52"></div>
 
 		<div id="inside-wrapper">
-			<label style="weight:500">Hamparan In Date</label>
-			<input class="w3-input w3-border border-radius-3" type="date" name="HampInDTTM" required />
+			<label style="font-weight:500">Estimate Date</label>
+			<input type="date" class="w3-border border-radius-3" name="estimate1" required />
 			<div class="height-10"></div>   
-			<label  style="weight:500">Hamparan Name</label>
+			<label style="font-weight:500">until</label>
+			<input type="date" class="w3-border border-radius-3" name="estimate2" required />
+			<div class="height-10"></div>
+			<label  style="font-weight:500">Monitoring Type</label>
 			<select class="w3-input w3-border border-radius-3" name="HampName" required />
 
 				<?php
@@ -26,7 +30,7 @@
 		<div class="height-20"></div>
 		<input type="submit" class="w3-btn w3-blue" name="register" value="Start Gather Data" />						
         
-    </form>
+	</form>
 	  
 	<div class="height-20"></div>
     <div id="loader-icon" class="border-radius-3" style="display:none;">..Gathering information, please wait</div>
