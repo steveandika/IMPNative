07c769a961c0fe7bb08d1dea62904a17aea64a8b
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-13 00:39:25 +0700

    Update testOOP.php

--- e-imp/asset/libs/testOOP.php
+++ e-imp/asset/libs/testOOP.php
@@ -2,7 +2,7 @@
 	include("new_db.php");
 	$obj = new DatabaseClass ();
 	
-	$rsl = get_rows("count(1) JumlahBaris" ,"userID='root'"  ,"userMenuProfile");
+	$rsl = $obj -> get_rows("count(1) JumlahBaris" ,"userID='root'"  ,"userMenuProfile");
 	echo "<pre>";  
 	print_r($rsl);  
 	echo "</pre>";  	
