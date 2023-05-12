9e3ea28ef3e60af20849564359078b1ffeb1f814
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-13 02:05:45 +0700

    Update mntreorvw.php

--- e-imp/fr/mntreorvw.php
+++ e-imp/fr/mntreorvw.php
@@ -9,7 +9,7 @@
 		$op = $_POST["HampName"];
 		if($op == "EoRIConS")
 		{
-			$rsl = $obj -> get_rows("shortName,workshopID" ,"userID='root'" ,"VIEW_MonitoringWaitingInv GROUP BY shortName,workshopID");
+			$rsl = $obj -> get_rows("shortName,workshopID" ,'' ,"VIEW_MonitoringWaitingInv GROUP BY shortName,workshopID");
 			include("layout1mntreor.php");
 		}
 	}
