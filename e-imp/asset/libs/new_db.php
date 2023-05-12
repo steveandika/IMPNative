2d8c880d58d8470062bf8b83d13b40c9cad9e0f5
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-12 23:43:42 +0700

    Update new_db.php

--- e-imp/asset/libs/new_db.php
+++ e-imp/asset/libs/new_db.php
@@ -1,7 +1,7 @@
 <?php  
 	$server='192.168.1.5, 1435'; 
   
-	$dbSQL = mssql_connect($server, 'sa', 'Pswd1209078');
+	$dbSQL = new mssql($server, 'sa', 'Pswd1209078');
 	if($dbSQL->connect_error) 
 	{ 
 		die('Connection failed: '.$dbSQL->connect_error); 
