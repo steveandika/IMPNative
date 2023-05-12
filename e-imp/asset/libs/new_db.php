99a2cb960c40991edb3070ee279c5eaeec79a8c9
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-12 23:20:17 +0700

    Create new_db.php

---
+++ e-imp/asset/libs/new_db.php
@@ -0,0 +1,13 @@
+<?php  
+	$server='192.168.1.5, 1435'; 
+  
+	$dbSQL = mssql_connect($server, 'sa', 'Pswd120907');
+	if($dbSQL->connect_error) 
+	{ 
+		die('Connection failed: '.$dbSQL->connect_error); 
+	} 
+	else 
+	{ 
+      echo "connected"; 
+    }
+?>
