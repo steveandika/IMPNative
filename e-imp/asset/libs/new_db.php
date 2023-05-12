ec612c5206045c9691842d57b93eb9eabffcd1ae
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-13 00:24:53 +0700

    Update new_db.php

--- e-imp/asset/libs/new_db.php
+++ e-imp/asset/libs/new_db.php
@@ -1,13 +1,46 @@
-<?php  
-	$server='192.168.1.5, 1435'; 
-  
-	$dbSQL = new mssql($server, 'sa', 'Pswd1209078');
-	if($dbSQL->connect_error) 
-	{ 
-		die('Connection failed: '.$dbSQL->connect_error); 
-	} 
-	else 
-	{ 
-      echo "connected"; 
-    }
+<?php 
+	class DatabaseClass  
+	{  
+		private $host = "192.168.1.5, 1435"; // your host name  
+		private $username = "user_imp"; // your user name  
+		private $password = "papaBr4v078"; // your password  
+		private $db = "CSSCY"; // your database name  
+		
+		public  
+		function __construct()  
+		{  
+			mssql_connect($this - > host, $this - > username, $this - > password) or die(mssql_get_last_message);  
+			mssql_select_db('CSSCY') or die(mssql_get_last_message);  
+		}
+		
+		// this method used to execute mysql query  
+		protected  
+		function query_executed($sql)  
+		{  
+			$c = mssql_query($sql);  
+			return $c;  
+		}  
+		
+		public  
+		function get_rows($fields, $id = NULL, $tablename = NULL)  
+		{  
+			$cn = !emptyempty($id) ? " WHERE $id " : " ";  
+			$fields = !emptyempty($fields) ? $fields : " * ";  
+			$sql = "SELECT $fields FROM $tablename $cn";  
+			$results = $this - > query_executed($sql);  
+			$rows = $this - > get_fetch_data($results);  
+			return $rows;  
+		}  
+    
+		protected  
+		function get_fetch_data($r)  
+		{  
+			$array = array();  
+			while ($rows = mssql_fetch_assoc($r))  
+			{  
+				$array[] = $rows;  
+			}  
+			return $array;  
+		}  
+	}  
 ?>
