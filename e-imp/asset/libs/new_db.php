9b6f798c9cf050b78a8959f984e5a429b83f980a
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-13 08:48:04 +0700

    Update new_db.php

--- e-imp/asset/libs/new_db.php
+++ e-imp/asset/libs/new_db.php
@@ -6,23 +6,20 @@
 		private $password = "papaBr4v078"; // your password  
 		private $db = "CSSCY"; // your database name  
 		
-		public  
-		function __construct()  
+		public function __construct()  
 		{  
 			mssql_connect($this -> host, $this -> username, $this -> password) or die(mssql_get_last_message);  
 			mssql_select_db('CSSCY') or die(mssql_get_last_message);  
 		}
 		
 		// this method used to execute mysql query  
-		protected  
-		function query_executed($sql)  
+		protected function query_executed($sql)  
 		{  
 			$c = mssql_query($sql);  
 			return $c;  
 		}  
 		
-		public  
-		function get_rows($fields, $id = NULL, $tablename = NULL)  
+		public function get_rows($fields, $id = NULL, $tablename = NULL)  
 		{  
 			$cn = !empty($id) ? " WHERE $id " : " ";  
 			$fields = !empty($fields) ? $fields : " * ";  
@@ -31,9 +28,16 @@
 			$rows = $this -> get_fetch_data($results);  
 			return $rows;  
 		}  
+		
+		public function get_listEoRWaitingBilled($fields)
+		{
+			$sql = "SELECT $fields FROM VIEW_MonitoringEstimateICONSGrouping";
+			$results = $this -> query_executed($sql);
+			$rows = $this -> get_fetch_data($results);
+			return $rows;
+		}
     
-		protected  
-		function get_fetch_data($r)  
+		protected function get_fetch_data($r)  
 		{  
 			$array = array();  
 			while ($rows = mssql_fetch_assoc($r))  
