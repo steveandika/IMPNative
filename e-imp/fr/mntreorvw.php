23ac68037d152dc0348086e954d85a7fe250640f
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-13 10:05:47 +0700

    Update mntreorvw.php

--- e-imp/fr/mntreorvw.php
+++ e-imp/fr/mntreorvw.php
@@ -8,8 +8,14 @@
 		$op = $_POST["HampName"];
 		if($op == "EoRIConS")
 		{
-			$rsl = $obj -> get_listEoRWaitingBilled();
+			$rsl = $obj -> get_listMntrEoRFin("VIEW_MonitoringEstimateICONSGrouping");
 			include("layout1mntreor.php");
+		}
+		
+		if($op == "EoRPDF")
+		{
+			$rsl = $obj -> get_listMntrEoRFin("VIEW_MonitoringEstimateICONSPDFGrouping");
+			include("layout1mntreor.php");			
 		}
 	}
 ?>
