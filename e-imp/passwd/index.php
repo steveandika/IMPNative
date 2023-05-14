652b1472f163a5f0e86c515149b11af5c42566e0
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-15 05:00:09 +0700

    Update index.php

--- e-imp/passwd/index.php
+++ e-imp/passwd/index.php
@@ -36,81 +36,75 @@
 
 <!DOCTYPE html>
 <html>
-<head>  
-	<meta charset="utf-8"> 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
-	<title>IMP | Integrated Container System</title>
-    <meta name="keywords" content="Container, Logistic, Container Repair, Indo Makmur">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<link rel="shortcut icon" href="../asset/img/office.png" type="image/x-icon" />  
-	<link rel="stylesheet" type="text/css" href="../asset/css/master.css" />  
-	<script src="../asset/js/modernizr.custom.js"></script>    
-	<script src="../asset/js/jquery.min.2.1.1.js"></script> 
-</head>
+	<head>  
+		<meta charset="utf-8"> 
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+		<meta name="keywords" content="Container, Logistic, Container Repair, Indo Makmur">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>IMP | Integrated Container System</title>
+		<link rel="shortcut icon" href="../asset/img/office.png" type="image/x-icon" />  
+		<link rel="stylesheet" type="text/css" href="../asset/css/master.css" />  
+		<script src="../asset/js/modernizr.custom.js"></script>    
+		<script src="../asset/js/jquery.min.2.1.1.js"></script> 
+	</head>
 
-<body> 
-	<?php 
-		if (!isset($_SESSION["uid"])) 
-		{
-			$url = "/"; 
-			echo "<script type='text/javascript'>location.replace('$url');</script>"; 
-		} 	
-		else {   
-			include ($_SERVER["DOCUMENT_ROOT"]."/e-imp/asset/libs/common.php"); 
-			openDB();
-			include ($_SERVER["DOCUMENT_ROOT"]."/e-imp/asset/libs/dashboard.php");
-			
-			if($newval == "") 
+	<body> 
+		<?php 
+			if (!isset($_SESSION["uid"])) 
 			{
-				include ("request_ch_pswd.php");
-			}
-			else {
-				if($url == "confirmed")
+				$url = "/"; 
+				echo "<script type='text/javascript'>location.replace('$url');</script>"; 
+			} 	
+			else 
+			{   
+				include ($_SERVER["DOCUMENT_ROOT"]."/e-imp/asset/libs/common.php"); 
+				openDB();
+				include ($_SERVER["DOCUMENT_ROOT"]."/e-imp/asset/libs/dashboard.php");
+		?>
+		
+		<div class="w3-container">
+			<div class="wrapper">
+
+		<?php
+				if($newval == "") 
 				{
-					include ("confirmed.php");
+					include ("request_ch_pswd.php");
 				}
-				else {
-					include ("failed.php");
+				else 
+				{
+					if($url == "confirmed")
+					{
+						include ("confirmed.php");
+					}
+					else 
+					{
+						include ("failed.php");
+					}
 				}
-			}
-	?>
+		?>
+			
+			</div>
+		</div>
 
- 
-	<?php
-		}
-	?>
-  
-<script>
-	var dropdown = document.getElementsByClassName("dropdown-btn");
-	var i;
-
-	for (i = 0; i < dropdown.length; i++) {
-		dropdown[i].addEventListener("click", function() {
-			this.classList.toggle("active");
-			var dropdownContent = this.nextElementSibling;
-			if (dropdownContent.style.display === "block") {
-				dropdownContent.style.display = "none";
+		<script>
+			function toggleSideNav() 
+			{		
+				if (document.getElementById("sideBar").style.left == "0px") 
+				{
+					document.getElementById("sideBar").style.left = "-250px";	
+				} 
+				else 
+				{
+					document.getElementById("sideBar").style.left = "0px";	
+				}		  
 			} 
-			else {
-				dropdownContent.style.display = "block";
+		</script> 
+		
+		<?php
 			}
-		});
-	}
-	
-	function toggleSideNav() 
-	{		
-		if (document.getElementById("sideBar").style.left == "0px") {
-			document.getElementById("sideBar").style.left = "-250px";	
-			document.getElementById("sideBar").style.width = "250px";	
-		} 
-		else {
-			document.getElementById("sideBar").style.left = "0px";	
-			document.getElementById("sideBar").style.width = "100%";
-		}		  
-	}     
-</script> 
+		?>
   
-</body>
+	</body>
 </html>  
