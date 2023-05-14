f7345e27464e657fe9d2646a69cffacd5ce8e823
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-15 05:16:39 +0700

    Update index.php

--- e-imp/index.php
+++ e-imp/index.php
@@ -37,6 +37,26 @@
 			</div>
 
 			<script>
+				var dropdown = document.getElementsByClassName("dropdown-btn");
+				var i;
+
+				for (i = 0; i < dropdown.length; i++) 
+				{
+					dropdown[i].addEventListener("click", function() 
+					{
+						this.classList.toggle("active");
+						var dropdownContent = this.nextElementSibling;
+						if (dropdownContent.style.display === "block") 
+						{
+							dropdownContent.style.display = "none";
+						} 
+						else 
+						{
+							dropdownContent.style.display = "block";
+						}
+					});
+				}
+				
 				function toggleSideNav() 
 				{		
 					if (document.getElementById("sideBar").style.left == "0px") 
