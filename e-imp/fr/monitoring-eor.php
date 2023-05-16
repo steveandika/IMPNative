3ba0289b5cf0f3a6ccc80450c4344bf20a8441ed
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-16 15:05:29 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -5,9 +5,7 @@
 	<div class="height-10"></div>
 	<div id="headerpage">
 		<div class="header-right">
-			<div class="w3-dropdown-content w3-bar-block w3-border border-radius-3">
-				<a class="w3-bar-item w3-button" href=<?php echo $defHTML."/e-imp/1?src='".base64_encode("fr/monitoring-eor.php")."'?sid=0" ?>>Eor IConS belum ditagihkan</a>
-			</div>
+          aaa
 		</div>
 	</div>
 	<div class="height-10"></div>
