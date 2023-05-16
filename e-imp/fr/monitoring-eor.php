7696d2f31f172b50031ede92c3bfd76626041f2c
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-16 20:47:16 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -1,14 +1,16 @@
-	<?php
-		 $defHTML = $_SESSION['defurl'];
-	?>
-	
 	<div class="height-10"></div>
 	<div class="wrapper-header">
+		<div class="title">Monitoring Penagihan EoR</div>
 		<div class="header-right">
-			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=0" ?>><b>EoR Belum Ditagihkan</b></a>
-			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=1" ?>><b>EoR (PDF) Belum Ditagihkan</b></a>
-			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=2" ?>><b>Menunggu ID Invoice </b></a>
-			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=3" ?>><b>Penagihan Complete</b></a>
+			<div class="w3-dropdown-hover">
+				<button class="w3-button w3-gray">Tipe Monitoring</button>
+				<div class="w3-dropdown-content w3-bar-block w3-border">
+			<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=0" ?>><b>EoR Belum Ditagihkan</b></a>
+			<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=1" ?>><b>EoR (PDF) Belum Ditagihkan</b></a>
+			<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=2" ?>><b>Menunggu ID Invoice </b></a>
+			<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=3" ?>><b>Penagihan Complete</b></a>				
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="height-10"></div>
