a364f922810fbdf29ee28188f6753c024c3d1bdb
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-16 16:42:04 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -5,7 +5,7 @@
 	<div class="height-10"></div>
 	<div class="wrapper-header">
 		<div class="header-right">
-			<a class="active" href="#user"><b>EoR Belum Ditagihkan</b></a>
+			<a href=<?php echo "'.$defHTML./e-imp/1?src=".base64_encode(fr/monitoring-eor.php).'"><b>EoR Belum Ditagihkan</b></a>
 		</div>
 	</div>
 	<div class="height-10"></div>
