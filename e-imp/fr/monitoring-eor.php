eabe37d4c3aed90a0b54ae4e76c236fc9031a6a2
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-16 17:00:33 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -6,6 +6,9 @@
 	<div class="wrapper-header">
 		<div class="header-right">
 			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=0" ?>><b>EoR Belum Ditagihkan</b></a>
+			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=1" ?>><b>EoR (PDF) Belum Ditagihkan</b></a>
+			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=2" ?>><b>Menunggu ID Invoice </b></a>
+			<a href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."?sid=3" ?>><b>Penagihan Complete</b></a>
 		</div>
 	</div>
 	<div class="height-10"></div>
