bb943ef0d06e3eb1159dd9d5dde502fae2212721
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-19 01:45:51 +0700

    Create mntreorlayout3.php

---
+++ e-imp/fr/mntreorlayout3.php
@@ -0,0 +1,42 @@
+	<?php		
+		$obj = new DatabaseClass ();
+		$rsl = $obj -> get_listMntrEoRFin("VIEW_MonitoringWaitingInv");
+	?>
+	
+	<h6 class='w3-text-blue-grey' style='font-weight:600;'>DOKUMEN PENAGIHAN Menunggu Invoice ID</h6>
+	<div class="height-10"></div>
+	<table class="w3-table w3-bordered">
+		<tr>
+			<th>Index</th>
+			<th></th>
+			<th>Document Number</th>
+			<th>Submit Date</th>
+			<th>Shipping Line</th>
+			<th>Party</th>
+			<th>Activity Type</th>
+			<th style="text-align:right;">Total Document of EoR</th>
+		</tr>
+			
+		<?php	
+			$index = 1;
+			for( $i = 0; $i < count($rsl); $i++ ) 
+			{
+				$html = "";
+				$html .= "<tr>";
+				$html .= "	<td>".$index."</td>";
+				$html .= "	<td></td>";
+				$html .= "	<td>".$rsl[$i]["DocNumber"]."</td>";
+				$html .= "	<td>".$rsl[$i]["SubmitDate"]."</td>";
+				$html .= "	<td>".$rsl[$i]["shortName"]."</td>";
+				$html .= "	<td>".$rsl[$i]["Party"]."</td>";
+				$html .= "	<td>".$rsl[$i]["activityType"]."</td>";
+				$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["jumlahEoR"], 2, ",",".")."</td>";
+				$html .= "</tr>";
+					
+				echo $html;
+				$index++;
+			}				
+		?>
+								
+	</table>
+	<div class="height-10"></div>
