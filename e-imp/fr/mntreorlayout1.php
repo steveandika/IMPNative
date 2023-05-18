0d3fba5b0da96859696bc66169c0da98377482d0
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-18 19:36:30 +0700

    Update mntreorlayout1.php

--- e-imp/fr/mntreorlayout1.php
+++ e-imp/fr/mntreorlayout1.php
@@ -15,28 +15,24 @@
 			<th style="text-align:right;">3rdParty</th>
 			
 			<?php	
-				$index = 0;
-				if(!isset($_SESSION["uid"]))
+				$index = 1;
+				for( $i = 0; $i < count($rsl); $i++ ) 
 				{
-					$index = 1;
-					for( $i = 0; $i < count($rsl); $i++ ) 
-					{
-						$html = "";
-						$html .= "<tr>";
-						$html .= "	<td>".$index."</td>";
-						$html .= "	<td>".$rsl[$i]["shortName"]."</td>";
-						$html .= "	<td>".$rsl[$i]["workshopID"]."</td>";
-						$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["Owner"], 2, ",",".")."</td>";
-						$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["User1"], 2, ",",".")."</td>";
-						$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["User2"], 2, ",",".")."</td>";
-						$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["ThirdParty"], 2, ",",".")."</td>";
-						$html .= "</tr>";
-					}
-							
+					$html = "";
+					$html .= "<tr>";
+					$html .= "	<td>".$index."</td>";
+					$html .= "	<td>".$rsl[$i]["shortName"]."</td>";
+					$html .= "	<td>".$rsl[$i]["workshopID"]."</td>";
+					$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["Owner"], 2, ",",".")."</td>";
+					$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["User1"], 2, ",",".")."</td>";
+					$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["User2"], 2, ",",".")."</td>";
+					$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["ThirdParty"], 2, ",",".")."</td>";
+					$html .= "</tr>";
+					
 					echo $html;
-					$i++;
 					$index++;
 				}
+					
 			?>
 								
 	</table>
