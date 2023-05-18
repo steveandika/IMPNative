a8b60be9a93bd0a0fe28fd55d8e6a7dcf50c5d2b
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-18 21:15:30 +0700

    Create mntreorlayout2.php

---
+++ e-imp/fr/mntreorlayout2.php
@@ -0,0 +1,36 @@
+	<?php		
+		$obj = new DatabaseClass ();
+		$rsl = $obj -> get_listMntrEoRFin("VIEW_MonitoringEstimateICONSPDFGrouping");
+	?>
+	
+	<h6 class='w3-text-blue-grey' style='font-weight:600;'>MONITORING EoR (PDF) - Belum Ditagihkan </h6>
+	<div class="height-10"></div>
+	<table class="w3-table w3-bordered">
+		<tr>
+			<th>Index</th>
+			<th></th>
+			<th>Shipping Line</th>
+			<th>Hamparan</th>
+			<th style="text-align:right;">Before Tax</th>
+			
+			<?php	
+				$index = 1;
+				for( $i = 0; $i < count($rsl); $i++ ) 
+				{
+					$html = "";
+					$html .= "<tr>";
+					$html .= "	<td>".$index."</td>";
+					$html .= "	<td></td>";
+					$html .= "	<td>".$rsl[$i]["shortName"]."</td>";
+					$html .= "	<td>".$rsl[$i]["workshopID"]."</td>";
+					$html .= "	<td style='text-align:right;'>".number_format($rsl[$i]["nilaiDPP"], 2, ",",".")."</td>";
+					$html .= "</tr>";
+					
+					echo $html;
+					$index++;
+				}
+					
+			?>
+								
+	</table>
+	<div class="height-10"></div>
