80a914a4e5cf1d7c3a8e1d1e6a5be61a5bba1d16
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-18 19:36:32 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -8,23 +8,24 @@
 	{ 
 ?>
 	
-	<div class="height-10"></div>
-	<div class="wrapper-header">
-		<div class="title">Monitoring Penagihan EoR</div>
+		<div class="height-10"></div>
+		<div class="wrapper-header">
+			<div class="title">Monitoring Penagihan EoR</div>
 		
-		<div class="header-right">
-			<div class="w3-dropdown-hover">
-				<button class="w3-button w3-gray">Tipe Monitoring</button>
-				<div class="w3-dropdown-content w3-bar-block w3-border">
-					<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=0" ?>>EoR Belum Ditagihkan</a>
-					<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=1" ?>>EoR (PDF) Belum Ditagihkan</a>
-					<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=2" ?>>Menunggu ID Invoice</a>
-					<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=3" ?>>Penagihan Complete</a>				
-				</div>
-			</div>		
+			<div class="header-right">
+				<div class="w3-dropdown-hover">
+					<button class="w3-button w3-gray">Tipe Monitoring</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=0" ?>>EoR Belum Ditagihkan</a>
+						<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=1" ?>>EoR (PDF) Belum Ditagihkan</a>
+						<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=2" ?>>Menunggu ID Invoice</a>
+						<a class="w3-bar-item w3-button" href=<?php echo "/e-imp/1?src=".base64_encode("fr/monitoring-eor.php")."&sid=3" ?>>Penagihan Complete</a>				
+					</div>
+				</div>		
+			</div>
 		</div>
-	</div>
-	<div class="height-10"></div>
+		<div class="height-10"></div>
+		<div class="w3-container w3-responsive w3-light-grey w3-round-large">
 			
 <?php
 		if(isset($_GET['sid']))
@@ -37,6 +38,9 @@
 			if($id == 2) {include("mntreorlayout3.php");}
 			if($id == 3) {include("mntreorlayout4.php");}*/
 		}
+		
+		
+		</div>
 	}
 ?>	
 
