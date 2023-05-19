646ad6070eb79fc0e06b775051eb6ebecda81e1b
steveandika <72459302+steveandika@users.noreply.github.com>
2023-05-19 07:26:55 +0700

    Update monitoring-eor.php

--- e-imp/fr/monitoring-eor.php
+++ e-imp/fr/monitoring-eor.php
@@ -31,9 +31,7 @@
 			if($id == 0) {include("mntreorlayout1.php");}
 			if($id == 1) {include("mntreorlayout2.php");}
 			if($id == 2) {include("mntreorlayout3.php");}
-			if($id == 3) {include("mntreorlayout4.php");}
-			
-			$rsl -> mssql_close();
+			if($id == 3) {include("mntreorlayout4.php");}						
 		}		
 	?>
 		
